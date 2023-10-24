4b6fb323061ad4af99634c43fd6adbf3a7af9cab
minorusso <gumbomb1@gmail.com>
2023-10-24 13:52:40 +0900

    validator

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"go-rest-api/model"
 	"go-rest-api/repository"
+	"go-rest-api/validator"
 	"os"
 	"time"
 
@@ -21,19 +22,18 @@ type IUserUsecase interface {
 // usecaseの構造体とrepositoryのインターフェースをつなぐ
 type userUsecase struct {
 	ur repository.IUserRepository
-	// uv validator.IUserValidator
+	uv validator.IUserValidator
 }
 
 // IUserUsecase型を満たすには定義したすべてのメソッドを追加しないといけない
-func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
-	return &userUsecase{ur}
+func NewUserUsecase(ur repository.IUserRepository, uv validator.IUserValidator) IUserUsecase {
+	return &userUsecase{ur, uv}
 }
 
 func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
-	//	if err := uu.uv.UserValidate(user); err != nil {
-	//		return model.UserResponse{}, err
-	//	}
-	//
+	if err := uu.uv.UserValidate(user); err != nil {
+		return model.UserResponse{}, err
+	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
 		return model.UserResponse{}, err
@@ -51,9 +51,9 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 }
 
 func (uu *userUsecase) Login(user model.User) (string, error) {
-	// if err := uu.uv.UserValidate(user); err != nil {
-	// 	return "", err
-	// }
+	if err := uu.uv.UserValidate(user); err != nil {
+		return "", err
+	}
 	storedUser := model.User{}
 	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
 		return "", err
